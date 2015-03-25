79206903458d5a48978d534b4aa603938698bf58
greye <greye@null.net>
2015-03-25 23:02:27 +0300

    [lua] move (presumably) common code to native.h

--- plugins/lua/lib/native.h
+++ plugins/lua/lib/native.h
@@ -1,11 +1,16 @@
 #ifndef NWNX_LUA_NATIVE_H
 #define NWNX_LUA_NATIVE_H
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define offset_ptr(ptr, off) ((void *) (((char *) (ptr)) + (off)))
+
 typedef struct CGameObject_s CGameObject;
+typedef void *(*CastFunctor)(CGameObject *);
 
 const char *
 get_script(CGameObject *object, int event);
